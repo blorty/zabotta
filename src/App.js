864ae0e4ab40e3a54852b45bc16b8b0ba07250b0
vadim l <vadimlozinschii@gmail.com>
2023-11-27 13:42:35 -0800

    legacy provider fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React from 'react';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+
+import Navbar from './components/Navbar/NavbarRender';
+import Home from './components/Home/HomeRender';
+import Footer from './components/Footer/FooterRender';
+
+
+const Container = styled.div`
+  background: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+
+// const Wrapper = styled.div`
+//   flex-grow: 1; 
+// `
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider>
+      <Navbar />
+      <Container>
+        <Home />
+        <Footer />
+      </Container>
+    </ThemeProvider>
+  )
 }
 
 export default App;
