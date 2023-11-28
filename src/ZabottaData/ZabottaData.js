91886bf8545f0a8fda6dade999a56fc88113575a
vadim l <vadimlozinschii@gmail.com>
2023-11-28 14:52:32 -0800

    Zabotta Services

--- src/ZabottaData/ZabottaData.js
+++ src/ZabottaData/ZabottaData.js
@@ -5,58 +5,80 @@ export const ZabottaData = {
     instagram: "https://www.instagram.com/zabottastudio/",
 }
 
+
 export const ZabottaServices = {
     services: [
         {
             id: 1,
-            title: "Flooring",
-            description: "We offer a wide range of flooring options for your home or business. We can help you choose the right flooring for your needs and budget.",
-            image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+            genre: "Remodeling",
+            options: [
+                {
+                    id: 1,
+                    title: "Flooring",
+                    description: "We offer a wide range of flooring options for your home or business. We can help you choose the right flooring for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                },
+                {
+                    id: 2, 
+                    title: "Kitchen Remodeling",
+                    description: "We offer a wide range of kitchen remodeling options for your home or business. We can help you choose the right kitchen remodeling for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                },
+                {
+                    id: 3,
+                    title: "Bathroom Remodeling",
+                    description: "We offer a wide range of bathroom remodeling options for your home or business. We can help you choose the right bathroom remodeling for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                },
+                {
+                    id: 4,
+                    title: "Tile Work",
+                    description: "We offer a wide range of tile work options for your home or business. We can help you choose the right tile work for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                }
+            ]
         },
         {
-            id: 2, 
-            title: "Kitchen Remodeling",
-            description: "We offer a wide range of kitchen remodeling options for your home or business. We can help you choose the right kitchen remodeling for your needs and budget.",
-            image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+            id: 2,
+            genre: "Custom Woodwork",
+            options: [
+                {
+                    id: 5,
+                    title: "Custom Cabinets",
+                    description: "We offer a wide range of custom cabinets options for your home or business. We can help you choose the right custom cabinets for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                },
+                {
+                    id: 6,
+                    title: "Custom Countertops",
+                    description: "We offer a wide range of custom countertop options for your home or business. We can help you choose the right custom woodwork for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                },
+                {
+                    id: 7,
+                    title: "Custom Wood Tables",
+                    description: "We offer a wide range of custom wood table options for your home or business. We can help you choose the right custom wood table for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1612838443371-1a0e4a6c1b4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29vZHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                }
+            ]
         },
         {
             id: 3,
-            title: "Bathroom Remodeling",
-            description: "We offer a wide range of bathroom remodeling options for your home or business. We can help you choose the right bathroom remodeling for your needs and budget.",
-            image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+            genre: "Upkeep & Maintenance",
+            options: [
+                {
+                    id: 8,
+                    title: "Home Upkeep",
+                    description: "We offer a wide range of home upkeep options for your home or business. We can help you choose the right home upkeep for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1612838443371-1a0e4a6c1b4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29vZHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                },
+                {
+                    id: 9,
+                    title: "Commercial Upkeep",
+                    description: "We offer a wide range of commercial upkeep options for your home or business. We can help you choose the right commercial upkeep for your needs and budget.",
+                    image: "https://images.unsplash.com/photo-1612838443371-1a0e4a6c1b4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29vZHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+                }
+            ]
         },
-        {
-            id: 4,
-            title: "Tile Work",
-            description: "We offer a wide range of tile work options for your home or business. We can help you choose the right tile work for your needs and budget.",
-            image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        },
-        {
-            id: 5,
-            title: "Custom Cabinets",
-            description: "We offer a wide range of custom cabinets options for your home or business. We can help you choose the right custom cabinets for your needs and budget.",
-            image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        },
-        {
-            id: 6,
-            title: "Custom Woodwork",
-            description: "We offer a wide range of custom woodwork options for your home or business. We can help you choose the right custom woodwork for your needs and budget.",
-            image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        },
-        {
-            id: 7,
-            title: "Room Renovation",
-            description: "We offer a wide range of room renovation options for your home or business. We can help you choose the right room renovation for your needs and budget.",
-            image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        },
-        {
-            id: 8,
-            title: "Upkeep & Maintenance",
-            description: "We offer a wide range of upkeep & maintenance options for your home or business. We can help you choose the right upkeep & maintenance for your needs and budget.",
-            image: "https://images.unsplash.com/photo-1581093458796-0c6b3b8d5b9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb3Jpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        }
     ]
 }
-
-
-
